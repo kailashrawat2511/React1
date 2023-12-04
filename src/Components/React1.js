b002eb7844eb587fb26ca0ea7021ae3cea3c61d5
Kailash Rawat <you@example.com>
2023-12-04 13:47:30 +0530

    change

---
+++ src/Components/React1.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function React1() {
+  return (
+    <>
+       Ques1:What is Virtual DOM?<br></br><br></br>
+
+Ans:We used Virtual DOM in react application. DOM is slow but virtual DOM is very fast. Its reload only what we want to reload in the entire website.<br></br> <br></br>
+
+Ques2:What is SPA?<br></br><br></br>
+
+Ans:SPA means SINGLE PAGE APLLICATION AND React is a single page apllication.<br></br><br></br>
+
+Ques3:What is the difference between class and functional component?<br></br><br></br>
+
+Ans:A functional component is just a plain JavaScript pure function that accepts props as an argument and returns a React element(JSX). A class component requires you to extend from React. Component and create a render function that returns a React element.<br></br><br></br>
+
+Ques4:What does mean by state and it's use in react?<br></br><br></br>
+
+Ans:It is used to contain data or information about the component. A component's state can change over time whenever it changes the component re-renders.<br></br><br></br>
+
+Ques5:What is JSX and why do we use it instead of JS?<br></br><br></br>
+
+Ans:JSX stands for Javscript Extension and we used JSX in react bcoz in JS we can't write html code but iN JSX we can write HTML code i.e, we used JSX in react.<br></br><br></br>
+
+Ques6:What is package.json?<br></br><br></br>
+
+Ans:It is used in react application. Package.json tells us which type of app name we created and version of react, react dom, react-router-dom,react-scripts and everything which is important for react app.<br></br> <br></br>
+
+Ques7:What is the Difference between react and react native? Which one is liary or framework?<br></br><br></br>
+
+Ans:React is a liary whereas React Native is a framework. React utilizes HTML, CSS, and JavaScript to create interactive user interfaces, whereas React Native utilizes APIs and UI components to build mobile applications. WEcan build single page app with the help of React whereas we can build IOS AND Android app wit the help of react native.<br></br><br></br>
+
+Ques8:What is the package name you are using for routing?<br></br><br></br>
+
+Ans:React router package name we are used for routing.
+
+
+    </>
+  )
+}
+
+export default React1
